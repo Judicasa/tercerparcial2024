ead0985df353bf95293e915a0f2cc0f70d9af9b1
Judicasa <judicasa2007@gmail.com>
2024-12-06 15:01:26 -0500

    pagina

--- controller.py
+++ controller.py
@@ -1,28 +1,49 @@
 import time
 import sqlite3 as sql
+# def createDB():# Crear o conectar a la BD 'autoconocimiento.db'
+#     conn = sql.connect("autoconocimientoJuanDiego.db")
+#     print("Base de datos creada o conectada correctamente.")
+#     conn.close()
 
-def createDB():
-    conn = sql.connect('autoconocimiento.db')
-    print("Base de datos de autoconocimiento creada")
-    conn.commit()
-    conn.close()
+# def createTable(): 
+#     conn = sql.connect("autoconocimientoJuanDiego.db")
+#     cursor = conn.cursor()
+#     cursor.execute("""
+#                    CREATE TABLE IF NOT EXISTS users (
+#                     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                     full_name TEXT NOT NULL,
+#                     email TEXT UNIQUE NOT NULL,
+#                     password_hash TEXT NOT NULL,
+#                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#                     ); """)
+#     print("tabla creada exitosamente")    
 
-def createTable():
-    conn = sql.connect('autconocimiento.db')
+def insertrow(user_id, full_name, email, password_hash, created_at):
+    try:
+        conn = sql.connect("autoconocimientoJuanDiego.db")
+        cursor = conn.cursor()
+        instruction = f"INSERT INTO users VALUES('{user_id}', '{full_name}', '{email}', '{password_hash}', '{created_at}')"
+        cursor.execute(instruction)
+        conn.commit()
+        print("Datos insertados correctamente")
+    except sql.operationalError as e:
+        time.sleep(1)
+        insertrow(user_id, full_name, email, password_hash, created_at)
+    finally:
+        conn.close()
+
+def readrow():
+    conn = sql.connect("autoconocimientoJuanDiego.db")
     cursor = conn.cursor()
-    cursor.execute("""
-    CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    full_name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    """)
-    print('Tabla creada')
+    instruction = f"SELECT * FROM users"
+    cursor.execute(instruction)
+    datos = cursor.fetchall()
     conn.commit()
     conn.close()
+    print(datos)
 
 if __name__ == "__main__":
-    createDB()
-    createTable()
+    #createDB()     # Primero creamos la base de datos
+    #createTable()
+    #insertrow(1, "Juan Diego", "juan@gmail.com", "123456", "2023-01-01")
+    readrow()
